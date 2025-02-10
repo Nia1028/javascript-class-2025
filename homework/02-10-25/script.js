1b65d642d9d2d835b1050208919592ee8b34f6a1
Nia <chaithanyapradeep003@gmail.com>
2025-02-10 01:17:43 -0600

    firstcommit

---
+++ homework/02-10-25/script.js
@@ -0,0 +1,75 @@
+//1.
+
+let age = prompt("how old are you?");
+if (age <= 0) {
+  console.log("Invalid Input.");
+} else if (age > 0 && age < 12) {
+  console.log(`The ticket price for the age ${age} is 5$.`);
+} else if (age > 12 && age < 18) {
+  console.log(`The ticket price for the age ${age} is 10$.`);
+} else if (age > 18 && age < 60) {
+  console.log(`The ticket price for the age ${age} is 20$.`);
+} else {
+  console.log(`The ticket price for the age ${age} is  15$.`);
+}
+
+//2.
+
+let number = prompt("Enter a number");
+let letter;
+if (number <= 0) {
+  console.log("Invalid Input.");
+} else if (number > 0 && number < 15) {
+  letter = "B";
+  console.log(
+    `The number ${number} falls under the letter "${letter}" on a Bingo card.`
+  );
+} else if (number > 15 && number < 30) {
+  letter = "I";
+  console.log(
+    `The number ${number} falls under the letter "${letter}" on a Bingo card.`
+  );
+} else if (number > 30 && number < 45) {
+  letter = "N";
+  console.log(
+    `The number ${number} falls under the letter "${letter}" on a Bingo card.`
+  );
+} else if (number > 45 && number < 60) {
+  letter = "G";
+  console.log(
+    `The number ${number} falls under the letter "${letter}" on a Bingo card.`
+  );
+} else {
+  letter = "O";
+  console.log(
+    `The number ${number} falls under the letter "${letter}" on a Bingo card.`
+  );
+}
+
+//3. 
+
+//4.
+
+//5.
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  //   console.log(`i= ${i}.`);
+  if (i % 3 == 0 || i % 7 == 0) {
+    sum = sum + i;
+  }
+}
+console.log(
+  `The sum of numbers between 1 and 100 that are divisible by 3 or 7 is: ${sum}`
+);
+
+//6.
+
+//7.
+
+let num = 100;
+let userInput;
+while (userInput != num) {
+  userInput = prompt("Guess the number:");
+}
+console.log("Congratulations! You guessed the correct number.");
--- script.js
+++ script.js
@@ -1,55 +1,152 @@
-//let age = prompt('How old are you?');
-//alert(age + ' is a good age');
-console.log(" Oh my aching back,\n might a swell claim workmans comp, \n it's thrown out again.");
+// //let age = prompt('How old are you?');
+// //alert(age + ' is a good age');
+// console.log(" Oh my aching back,\n might a swell claim workmans comp, \n it's thrown out again.");
 
-// CONCATINATION //
-console.log("Bob and Sheila " + "ate some donuts.");
+// // CONCATINATION //
+// console.log("Bob and Sheila " + "ate some donuts.");
 
-let numOfStudents =23;
-console.log("There are " + numOfStudents + " students in my class");
+// let numOfStudents =23;
+// console.log("There are " + numOfStudents + " students in my class");
 
-numOfEmployees = 6;
-console.log(`My company has ${numOfEmployees} employes on my floor.`); 
+// numOfEmployees = 6;
+// console.log(`My company has ${numOfEmployees} employes on my floor.`);
+
+// // DATA TYPES //
+// console.log("740_991 + 100_000"); // We can add underscore to make numbers easy to read //
+
+// let a = 9_007_199_254_740_991;
+// let b = 100_000;
+// console.log(a + b); //9_007_199_254_840_991
+//                     // but got 9_007_199_254_840_992 // using bigInt (9_007_199_254_840_991n)
+
+// console.log(0.2 + 0.1);   //expecting 0.3 but got 0.30000000000000004 to solve this
+// let x = 0.2;
+// let y = 0.1;
+// console.log((x * 10 + y * 10) / 10); // expecting 0.3 , got 0.3!
+
+// console.log(typeof ("Bob"));
+// console.log(typeof (23));
+// let c = "Charlie";
+// console.log(typeof (c));
+// console.log(typeof (NaN));
+// console.log(typeof 32n);
+// console.log(typeof undefined);
+
+// let d = 45;
+// let e =25;
+// let f = "15";
+
+// console.log("The result of d + e is " + d + e);
+// console.log("The result of (d + e) is " + (d + e));
+// console.log("The result of d + e + f is " + (d + e + f));
+// console.log("The result of d + e + f is " + ((d + e) + f));
+// console.log("The result of d + e + f is " + (d + ( e + f)));
+
+// let t = 5;
+// let s = t++;
+// console.log(t);
+// console.log(s);
+
+// let l = 5;
+// let m = ++l;
+// console.log(l);
+// console.log(m);
+
+// function addTwoNumbers(num1,num2){
+//     alert(num1 + num2);
+// }
+// addTwoNumbers(8 , 16);
+
+// function getTriangleArea(base,height){
+//     let area =0.5 * base *height;
+//     // alert(`The area of the triangle is ${area}`);
+//     return area;
+// }
+
+// // }
+// // getTriangleArea(5,10);
+
+// let base1 =prompt('What is the base of the triangle?');
+// let height1 =prompt('What is the height of the triangle?');
+// let area1 =getTriangleArea(base1, height1);
+// // getTriangleArea(base1, height1);
+// alert(`The area of your triangle is ${area1}`)
+
+// // CONTROL FLOW
+
+let pocketChange = 5.63;
+let lunchTimeLeft = 23;
+let hasCheckbook = true;
+
+// if (pocketChange > 5 && lunchTimeLeft > 20){
+//     console.log('I will have a ham and swiss please');
+// } else{
+//     console.log('I will eat later')
+// }
+
+// AND OPERATOR
+// 1. true 2.true == true
+// 1.false 2.true == false
+// 1.true  2.false ==false
+// 1.false 2.false ==false
+
+// OR OPERATOR
+// 1.true  2.true == true
+// 1.false 2.true == true
+// 1.true  2.false ==true
+// 1.false 2.false ==false
+
+// if (pocketChange > 5 || lunchTimeLeft > 30) {
+//   console.log("I will have a ham and swiss please");
+// } else {
+//   console.log("I guess I will just have soup :(");
+// }
 
 
-// DATA TYPES //
-console.log("740_991 + 100_000"); // We can add underscore to make numbers easy to read //
+// if (pocketChange > 5){
+//     console.log("I will have a ham and swiss please");
+// }else if (hasCheckbook){
+//     console.log("Will you take  a check?"); // to check more than one conditions
+// }
 
-let a = 9_007_199_254_740_991;
-let b = 100_000;
-console.log(a + b); //9_007_199_254_840_991
-                    // but got 9_007_199_254_840_992 // using bigInt (9_007_199_254_840_991n)
+// let testScore = prompt("What was your score on the test?");
 
-console.log(0.2 + 0.1);   //expecting 0.3 but got 0.30000000000000004 to solve this   
-let x = 0.2;
-let y = 0.1;
-console.log((x * 10 + y * 10) / 10); // expecting 0.3 , got 0.3!
+// if  (testScore >= 90) {
+//     console.log(`Terrific! You got an A!`);
+// }else if (testScore < 90 && testScore >= 80){
+//     console.log(`Good Job! You got a B!`);
+// }else if (testScore < 80 && testScore >= 70){
+//     console.log(`Not bad. You got a C.`);
+// } else if (testScore < 70 && testScore >= 60){
+//     console.log(`Not bad. You got a C!`);
+// }else{
+//     console.log("An F");
+// } 
+// SWITCH CONDITIONS
+// let grade = "B";
 
-console.log(typeof ("Bob"));
-console.log(typeof (23));
-let c = "Charlie";
-console.log(typeof (c));
-console.log(typeof (NaN)); 
-console.log(typeof 32n);
-console.log(typeof undefined);
+// switch (grade){
+//     case "A":
+//         console.log("Excellent!!");
+//         break;
+//     case "B":
+//         console.log("Great!!");
+//         break; 
+//     case "C":
+//         console.log("Not Bad.");
+//         break;  
+//     default:
+//         console.log("You Fail");      
+// }
 
+//LOOPS
 
-let d = 45;
-let e =25;
-let f = "15";
-
-console.log("The result of d + e is " + d + e);
-console.log("The result of (d + e) is " + (d + e));
-console.log("The result of d + e + f is " + (d + e + f));
-console.log("The result of d + e + f is " + ((d + e) + f));
-console.log("The result of d + e + f is " + (d + ( e + f)));
-
-let t = 5;
-let s = t++;
-console.log(t);
-console.log(s);
-
-let l = 5;
-let m = ++l;
-console.log(l);
-console.log(m);
+let age = prompt("how old are you?");
+let count = 0;
+while (age >= 1){
+   if (age % 3 === 0) {
+    count++;
+   }
+   age--;
+}
+console.log(`There are ${count} multiples of three between 1 and your age`); 
