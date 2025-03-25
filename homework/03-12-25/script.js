43f3e195995a0680015c034fc131b28429c29856
Nia <chaithanyapradeep003@gmail.com>
2025-03-25 12:20:10 -0500

    changes

--- homework/03-12-25/script.js
+++ homework/03-12-25/script.js
@@ -9,6 +9,7 @@ console.log(greetByName("Nia"));
 //SCOPE
 
 let x = 10;
+4;
 console.log(`Top of program - x is ${x}`); //10 - Global scope
 function globalScope() {
   x = 20;
@@ -17,3 +18,18 @@ function globalScope() {
 console.log(`Outside of function - x is ${x}`); //10 - Block Scope
 globalScope();
 console.log(`After calling function - x is ${x}`); //20 - Global Scope
+
+//STACK CALL
+
+console.log("This is the top of the program");
+
+function firstCall() {
+  console.log("This is the first function");
+}
+function secondCall() {
+  console.log("This is the second function");
+}
+function thirdCall() {
+  console.log("This is the third function");
+}
+firstCall();
